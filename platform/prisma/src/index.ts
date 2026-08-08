561bd25b262967eb31c6c7c1d4d5669c0fdbc29d
Arav menon <aravmenon.ak@gmail.com>
2026-08-09 03:40:47 +0530

    Refactor Prisma client initialization to include adapter and singleton pattern

--- platform/prisma/src/index.ts
+++ platform/prisma/src/index.ts
@@ -1,1 +1,12 @@
-export { PrismaClient } from "./generated/prisma/client.js";
+import {PrismaClient} from "./generated/prisma/client.js";
+import {PrismaPg} from "@prisma/adapter-pg"
+
+let _prisma: PrismaClient | null = null;
+
+export function getPrisma(): PrismaClient {
+  if (!_prisma) {
+    const adapter = new PrismaPg({connectionString: process.env.DATABASE_URL})
+    _prisma = new PrismaClient({adapter})
+  }
+  return _prisma
+}
