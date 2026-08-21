781b060f12e6368a80004967e3d2ff62bd70976d
Arav menon <aravmenon.ak@gmail.com>
2026-08-22 03:38:59 +0530

    feat: implement session management with caching and database integration in AgentSessionManager

--- packages/realtime/src/modules/AgentSessionManager.ts
+++ packages/realtime/src/modules/AgentSessionManager.ts
@@ -1,5 +1,71 @@
+import type { WebSocket } from "ws";
+import { db } from "../lib/prisma.js";
+import { cacheData } from "../lib/cache.js";
+import { CACHE_TTL, cacheKeys } from "../lib/cacheKey.js";
+
+export interface AgentCallData {
+    userId: string;
+    sessionId?: string | undefined;
+    repositoryId?: string;
+    prompt: string;
+    socket: WebSocket;
+}
+
 export class AgentSessionManager {
-    broadCast() {
-        console.log("Hii")
+    async SessionCheck({ sessionId, repositoryId, userId, prompt, socket }: AgentCallData): Promise<void> {
+        let session;
+
+        if (sessionId == undefined) {
+
+            session = await db.agentSession.create({
+                data: {
+                    userId,
+                    title: prompt.length > 30
+                        ? prompt.slice(0, 30) + "..."
+                        : prompt,
+                    repositoryId: repositoryId ?? null
+                }
+            });
+
+            await cacheData.deleteCache(cacheKeys.userSessionsRecent(userId));
+            await cacheData.setCache(cacheKeys.session(session.id), session, CACHE_TTL.SESSION);
+        } else {
+            const cacheKey = cacheKeys.session(sessionId);
+            const checkSessionCache = await cacheData.getCache(cacheKey);
+
+            if (checkSessionCache && checkSessionCache.userId === userId) {
+                session = checkSessionCache;
+            } else {
+                session = await db.agentSession.findFirst({
+                    where: {
+                        id: sessionId,
+                        userId
+                    }
+                });
+
+                if (session) {
+                    await cacheData.setCache(cacheKey, session, CACHE_TTL.SESSION);
+                }
+            }
+        }
+
+        if (!session) {
+            socket.send(JSON.stringify({ type: "error", message: "Session not found" }));
+            return;
+        }
+
+        const data: AgentCallData = {
+            userId: userId,
+            sessionId: session.id,
+            repositoryId: session.repositoryId,
+            prompt: prompt,
+            socket: socket,
+        };
+
+        this.CallToAgent(data);
+    }
+
+    CallToAgent(data: AgentCallData) {
+        data.socket.send(JSON.stringify("Call to agent"))
     }
 }
