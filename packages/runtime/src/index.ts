63c09deea7e9aa24b77e17c5930a68588ae122de
Arav menon <aravmenon.ak@gmail.com>
2026-09-01 18:57:57 +0530

    feat: initialize and bind gRPC server in runtime entry point and update proto path mapping

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,1 +1,10 @@
-console.log("KlinPi runtime");
+import {server} from "./server-RPC/rpc-server.js"
+import * as grpc from "@grpc/grpc-js";
+
+server.bindAsync("0.0.0.0:50051", grpc.ServerCredentials.createInsecure(), (error, port) => {
+    if (error) {
+        console.error(error);
+        return
+    }
+    console.log(`gRPC server is running on port ${port}`)
+})
