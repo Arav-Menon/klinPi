ea3108378c56f065b51355e0bc6f9f68e546a11f
Arav menon <aravmenon.ak@gmail.com>
2026-09-22 13:32:59 +0530

    fix(runtime): resolve proto package namespace for gRPC service registration

--- packages/runtime/src/server-RPC/rpc-server.ts
+++ packages/runtime/src/server-RPC/rpc-server.ts
@@ -2,6 +2,7 @@ import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import { handleRunAgent } from "./agent-handler.js";
 
 const _filename = fileURLToPath(import.meta.url);
 const _dirname = path.dirname(_filename);
@@ -11,8 +12,10 @@ const PROTO_PATH = path.join(_dirname, "../../../contracts/agent.proto");
 const packageDefinition = protoLoader.loadSync(PROTO_PATH);
 const grpcObject = grpc.loadPackageDefinition(packageDefinition);
 
-export const agentService = grpcObject.AgentService as any;
+const agentService = (grpcObject as any).agent.AgentService;
 
 export const server = new grpc.Server();
 
-// Now from here we need to send the data to the agent
+server.addService(agentService.service, {
+    RunAgent: handleRunAgent,
+});
